e24a72c21e6492c39d398ded850dab245ef5721f
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2021-11-29 18:19:10 +0400

    url update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,7 +12,7 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 private val gson = Gson()
-private const val BASE_URL = "http://10.0.2.2:9998"
+private const val BASE_URL = "http://127.0.0.1:9999"
 private val client = OkHttpClient.Builder()
     .addInterceptor(HttpLoggingInterceptor(::println).apply {
         level = HttpLoggingInterceptor.Level.BODY
